f6453a3c81a0d0a666f4f442c33218355842195b
Lucho <lefcgis@users.noreply.github.com>
2023-07-06 13:35:58 -0500

    Add files via upload
    By Lucho

---
+++ etiquetas personalizadas.js
@@ -0,0 +1,14 @@
+<style>
+	h4{font-size:20pt;margin:10;width:180}
+</style>
+<h4>[% nombdist %]</H4>
+<hr>
+<div><b>Provincia: </b>[% "nombprov" %]</div>
+<hr>
+<div><b>Departamento: </b>[% "nombdep" %]</div>
+<hr>
+<div><b>Ubigeo: </b>[% "ubigeo" %]</div>
+<hr>
+<div><b>Fuente de información: </b>INEI, 2023</div>
+<hr>
+<div><b>Miniworkshop por: </b>Asociación QGIS Perú</div>
